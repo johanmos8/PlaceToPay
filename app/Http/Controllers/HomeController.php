550db2f3ca0d76b4db244eceedbe6bf6f7ca8ac4
Johan Mosuqera <johanmosquera@unicauca.edu.co>
2020-12-08 22:01:49 -0500

    Feat: creation of Order migration
    creation of Controllers and initial Test files

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,8 +8,24 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function __invoke(){
+        
 
-        return View("home");
+        return View("home.home");
 
+    }
+    //
+
+    public function index(){
+
+        return view("courses.index");
+    }
+
+    public function create(){
+        return view("courses.create");
+    }
+
+    public function showProducts($curso){
+
+        return view('home.producto',['course' => $curso]);
     }
 }
---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.template')
+
+@section('title','Home')
+
+@section('content')
+<h1>Bienvenido a la página principal</h1>
+@endsection
