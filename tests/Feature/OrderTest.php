58d9f3a37858038753ac5a267b4a4cd1b39df81f
Johan Mosuqera <johanmosquera@unicauca.edu.co>
2020-12-09 23:49:32 -0500

    Feat: define some unit test to be used

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+
 
 class OrderTest extends TestCase
 {
@@ -22,7 +24,35 @@ class OrderTest extends TestCase
     /**
      * @test
      */
-    public function sendOrder(){
+    public function create_order_with_wrong_data(){
 
     }
+    public function create_order_sucessfully(){
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+          //Request to create order with product id and user logged
+          $response = $this->actingAs($user)->post('/createOrder', 
+          [
+              "customer_name"=>"1",
+              "customer_email"=>"",
+              "customer_mobile"=>"",
+              "product_id"=>""
+          ]);
+  
+          //Petition success ?
+          $response->assertStatus(302);
+           
+          // Check new order in the database whit user_id
+          $this->assertDatabaseHas('orders',[
+              "user_id" => $user->id,
+          ]);
+    }
+    public function retry_payment_sucessfully(){
+
+    }
+    public function view_my_orders(){
+
+    }
+ 
 }
---
+++ tests/Feature/Payment/PaymentTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Payment;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PaymentTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function can_not_invoke(){
+        
+    }
+}
