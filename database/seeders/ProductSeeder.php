7a58e6e0893fb609c98b9b5cd399ea23bdfed7d2
Johan Mosuqera <johanmosquera@unicauca.edu.co>
2020-12-13 20:27:03 -0500

    Fix: fixed seeder error

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -19,7 +19,7 @@ class ProductSeeder extends Seeder
         $product->name="Fc Barcelona";
         $product->description="Oficial jersey for season 2021";
         $product->image="images/barcelona.jpg";
-        $product->price="$19.99";
+        $product->price="19.99";
         $product->save();
 
         //it's added new product to generate orders
@@ -27,7 +27,7 @@ class ProductSeeder extends Seeder
         $product->name="Real Madrid FC";
         $product->description="Oficial jersey for season 2021";
         $product->image="images/real_madrid.png";
-        $product->price="$19.99";
+        $product->price="19.99";
         $product->save();
 
         //it's added new product to generate orders
@@ -35,7 +35,7 @@ class ProductSeeder extends Seeder
         $product->name="Atalanta";
         $product->description="Oficial jersey for season 2021";
         $product->image="images/atalanta.png";
-        $product->price="$19.99";
+        $product->price="19.99";
         $product->save();
     }
 }
